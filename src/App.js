ea2207714cbdfeb84640c0ff274d90b541606e74
AParnaby <parnaby.adam@gmail.com>
2023-01-22 18:16:46 +0000

    Initial form design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
+
+const requests = [
+  {
+    value: 'Underling',
+    label: 'Underling',
+  },
+  {
+    value: 'Missive',
+    label: 'Missive',
+  },
+  {
+    value: 'Destination',
+    label: 'Request Adding Destination to Board',
+  },
+  {
+    value: 'Excursion',
+    label: 'Start Excursion',
+  },
+  {
+    value: 'Audience',
+    label: 'Start Audience',
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>//.Symposium_Host_Access</h1>
+      <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '80ch' },
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+        color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
+        border: '1px solid',
+        borderColor: (theme) =>
+          theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+        p: 1,
+        borderRadius: 2,
+        fontSize: '0.875rem',
+        fontWeight: '700',
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Stack spacing={2}>
+      <TextField id="character-name" label="Name" variant="outlined" />
+      <TextField id="request-type" select label="Request Type" defaultValue="Underling" helperText="Please select your request type">
+        {requests.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+        ))}
+      </TextField>
+      <TextField
+          id="outlined-multiline-flexible"
+          label="Request Information"
+          multiline
+          minRows={4}
+          maxRows={40}
+        />
+      <Button variant="contained">Submit Request</Button>
+      </Stack>
+      </Box>
       </header>
     </div>
+    
   );
 }
 
